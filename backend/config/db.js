3131bbe45fc01e74594106d6608f77f2da743b6c
Martin Spassov <mspassov@uwo.ca>
2022-03-06 10:47:25 -0500

    connected to Mongo Database

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const { builtinModules } = require('module');
+const mongoose = require('mongoose');
+
+const connectDB = async () =>{
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Mongo DB is connected ${connect.connection.host}`)
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
